c1a9c64b6b12aed789e9d6d870818b5538481c96
zhangph <15858331351@163.com>
2017-02-27 11:19:34 +0800

    断路由

--- Chapter9-1-2/eureka-feign/src/main/java/com/didispace/service/ComputeClient.java
+++ Chapter9-1-2/eureka-feign/src/main/java/com/didispace/service/ComputeClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("compute-service")
 public interface ComputeClient {
-
+	
     @RequestMapping(method = RequestMethod.GET, value = "/add")
     Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
 
--- Chapter9-1-2/eureka-feign/src/main/java/com/didispace/web/ConsumerController.java
+++ Chapter9-1-2/eureka-feign/src/main/java/com/didispace/web/ConsumerController.java
@@ -1,6 +1,8 @@
 package com.didispace.web;
 
 import com.didispace.service.ComputeClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +13,15 @@ public class ConsumerController {
 
     @Autowired
     ComputeClient computeClient;
-
+    
+    @HystrixCommand(fallbackMethod = "addServiceFallback")//加一个断路由，如果程序出错不会一直阻塞，调用一个出错时候的机制放啊
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add() {
         return computeClient.add(10, 20);
+    }
+    
+    public Integer addServiceFallback() {
+        return 102;
     }
 
 }
